34399787300a4359cc9b83aed1973ce1cd558d56
iantoniou <antoniouioannis10@gmail.com>
2026-03-17 00:22:36 +0200

    some photos

--- lib/pages/section_authentication/auth2/auth2_widget.dart
+++ lib/pages/section_authentication/auth2/auth2_widget.dart
@@ -303,7 +303,7 @@ class _Auth2WidgetState extends State<Auth2Widget>
                                 fit: BoxFit.scaleDown,
                                 alignment: AlignmentDirectional(0.0, -1.0),
                                 image: Image.asset(
-                                  'assets/images/global.jpeg',
+                                  'assets/images/somon.jpg',
                                 ).image,
                               ),
                               boxShadow: [
